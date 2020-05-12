21a4dff0ca46a1634e06490a4170c1540229344e
Arunav Gupta <centaurus.ab@gmail.com>
2020-05-12 00:50:11 -0700

    tree generation works

--- GraphTreeWalk.py
+++ GraphTreeWalk.py
@@ -6,54 +6,108 @@ import math
 from sklearn.cluster import KMeans
 
 class TreeNode:
-    """Node object for cluster tree."""
-    
-    def __init__(self, parent, graph_nodes):
+    def __init__(self, tracts=None):
+        self.tracts = tracts
+        self.left = None
+        self.right = None
+
+class ClusterTree:
+    def __init__(self, all_tracts=None):
+        self.root = TreeNode(all_tracts)
+        self.size = 0
+
+    def add(self, tracts):
         """
-        Creates new node.
+        Adds new node to tree. Sorting into tree is done by subset
+        i.e. root is 'ABCDE', right is 'DE', left is 'ABC',
+        then a new node consisting of 'BC' would be placed as a right
+        child of 'ABC', since it is a subset of that list and is > 'ABCDE'
+
         Parameters:
-        parent - TreeNode directly above new node
-        graph_nodes - list of graph nodes to add to this node
-        
+        tracts - List of tract_IDs to include in this node
         """
-        # self.children should contain no more than 2 TreeNode objects
-        self.children = []
-        self.parent = parent
-        parent.children.append(self)
-        self.graph_nodes = graph_nodes
-        self.conn_to_sibling = False
-        self.leaves_connected = False
-    
-    def update_status(self):
-        """
-        1. Change conn_to_sibling to true if all nodes in graph_nodes
-        are connected to sibling at same level.
-        2. Change leaves_connected if both child nodes are connected, or
-        if children list is empty (node is a leaf)
-        """
-        assert(len(self.children) <= 2)
-        
-        if (self.is_leaf()):    
-            self.leaves_connected = True
-        if (self.children[0].conn_to_sibling and self.children[1].conn_to_sibling):
-            self.leaves_connected = True
-        
-        self.get_sibling().graph_nodes
-         
-    def get_sibling(self):
-        """Returns sibling of current node."""
-        return [n for n in self.parent.children if n != self][0]
-    
-    def is_leaf(self):
-        """Returns true if node is a leaf (no children)."""
-        return not self.children
-        
-tract_coords = pd.DataFrame({'tract_ID': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'], 'X': [1, 2, 4, 3, 5, 10, 0, 9, 0, 0, 7, 5, 2, 7, 0, 9], 'Y': [3, 1, 3, 5, 6, 11, 9, 0, 1, 0, 3, 10, 8, 8, 10, 5]})
+        # create new Node to house tracts
+        newNode = TreeNode(tracts)
+        # helper function to check if new list is subset of parent one
+        isSubset = lambda sub, main: all(x in main for x in sub)
+        # make sure all tracts are part of main set
+        assert(isSubset(tracts, self.root.tracts))
 
-for i in range(int(math.log2(len(tract_coords.tract_ID)))):
-    clusters = KMeans(n_clusters = 2*(i+1)).fit_predict(np.column_stack([tract_coords.X.values, tract_coords.Y.values]))
-    tract_coords['cluster_{}'.format(i+1)] = clusters
-    plt.scatter(tract_coords.X, tract_coords.Y, c=tract_coords['cluster_{}'.format(i+1)])
-    plt.show()
+        currNode = self.root
+        while (currNode != None):
+            # assert(isSubset(tracts, currNode.tracts), "Node insertion failed: \
+            # tracts aren't subset of parent node tracts")
+            if (currNode.left == None and currNode.right == None):
+                if (tracts < currNode.tracts):
+                    currNode.left = newNode
+                    currNode = None
+                elif (tracts > currNode.tracts):
+                    currNode.right = newNode
+                    currNode = None
+            elif (currNode.left == None):
+                currNode.left = newNode
+                currNode = None
+            elif (currNode.right == None):
+                currNode.right = newNode
+                currNode = None
+            else:
+                if (isSubset(tracts, currNode.left.tracts)):
+                    currNode = currNode.left
+                else:
+                    currNode = currNode.right
 
-tract_coords
+    # A function to do postorder tree traversal
+    def printPostorder(self, root):
+
+        if root:
+            print(root.tracts)
+            self.printPostorder(root.left)
+            self.printPostorder(root.right)
+
+
+
+def assemble_tree():
+    global tract_coords
+    # create tree with root being list of all tract_IDs
+    tree = ClusterTree(list(tract_coords.tract_ID.values))
+
+    # cluster through recursively and divide into two until each cluster has 1-2 tracts
+    clustering(tree, tree.root.tracts)
+
+    return tree
+
+
+def clustering(tree, tracts):
+    global tract_coords
+    if (len(tracts) < 3):
+        # create new Node with root.tracts
+        return 0
+    else:
+        # split into 2 groups using clustering
+        tracts_to_split = tract_coords.loc[tract_coords.tract_ID.isin(tracts)]
+        clusters = KMeans(n_clusters=2).fit_predict(np.column_stack([tracts_to_split.X.values, tracts_to_split.Y.values]))
+        tracts_to_split['cluster'] = clusters
+        group_a = tracts_to_split[tracts_to_split.cluster == 0]
+        group_b = tracts_to_split[tracts_to_split.cluster == 1]
+
+        # create 2 new nodes and add to tree, with groups
+        tree.add(list(group_a.tract_ID.values))
+        tree.add(list(group_b.tract_ID.values))
+
+        # cluster both new groups
+        clustering(tree, list(group_a.tract_ID.values))
+        clustering(tree, list(group_b.tract_ID.values))
+
+
+tract_coords = pd.DataFrame({'tract_ID': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                          'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
+                             'X': [1, 2, 4, 3, 5, 10, 0, 9, 0, 0, 7, 5, 2, 7, 0, 9],
+                             'Y': [3, 1, 3, 5, 6, 11, 9, 0, 1, 0, 3, 10, 8, 8, 10, 5]})
+
+
+def main():
+    cluster_tree = assemble_tree()
+    cluster_tree.printPostorder(cluster_tree.root)
+
+if __name__ == '__main__':
+    main()
