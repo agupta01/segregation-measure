42dfff700b00fe180158ef9719f2d4a485629ea6
Arunav Gupta <centaurus.ab@gmail.com>
2020-06-07 22:36:05 -0700

    graphprep begun

--- GraphTreeWalk.py
+++ GraphTreeWalk.py
@@ -8,11 +8,12 @@ from shapely import ops
 from shapely.geometry import Point, LineString, MultiLineString
 from tqdm import tqdm
 from shapely import speedups
+
 speedups.enable()
 
 
 class TreeNode:
-    def __init__(self, tracts=None):
+    def __init__(self, tracts=None, is_leaf=False):
         self.tracts = tracts
         self.left = None
         self.right = None
@@ -22,6 +23,7 @@ class TreeNode:
         self.graph = []
         # line that connects subgraph to another subgraph
         self.connecting_line = None
+        self.is_leaf = is_leaf
 
     def graph_node(self, save=None):
         """
@@ -35,8 +37,8 @@ class TreeNode:
         global tract_coords
         fig, ax = plt.subplots()
         ax.scatter(tract_coords.X, tract_coords.Y, alpha=0.6)
-        # for i in range(len(self.tracts)):
-        #     ax.text(tract_coords.X[i], tract_coords.Y[i], tract_coords.tract_ID[i])
+        for i in range(len(self.tracts)):
+            ax.text(tract_coords.X[i], tract_coords.Y[i], tract_coords.tract_ID[i])
         for line in self.graph:
             x, y = line.xy
             ax.plot(x, y, color='grey', alpha=0.5, linewidth=1, solid_capstyle='round', zorder=2)
@@ -50,7 +52,6 @@ class TreeNode:
 class ClusterTree:
     def __init__(self, all_tracts=None):
         self.root = TreeNode(all_tracts)
-        self.size = 0
 
     def add(self, tracts):
         """
@@ -63,7 +64,7 @@ class ClusterTree:
         tracts - List of tract_IDs to include in this node
         """
         # create new Node to house tracts
-        newNode = TreeNode(tracts)
+        newNode = TreeNode(tracts, is_leaf=True) if len(tracts) <= 2 else TreeNode(tracts)
         # add lines and heads if leaf node
         if len(tracts) == 2:
             newNode.heads = tracts
@@ -158,14 +159,42 @@ def merge(child_node_a, child_node_b):
 
     if (len(merge_pairs) == 0):  # no "visible" connections, have to go back and redo merge children for both nodes
         # first, need to remove old lines from children's left & right
-        for child in [child_node_a, child_node_b]:
-            # child.left.graph.remove(child.left.connecting_line)
-            child.left.connecting_line = None
-            # child.right.graph.remove(child.right.connecting_line)
-            child.right.connecting_line = None
-        raise RuntimeError("Trapped case reached!")
-        merge(child_node_a.left, child_node_a.right)
-        merge(child_node_b.left, child_node_b.right)
+        # raise RuntimeError("Trapped case reached!")
+        print("Backtracking!")
+        if not child_node_a.is_leaf:
+            child_node_a.graph = []
+            child_node_a.heads = []
+            child_node_a.left.connecting_line = None
+            child_node_a.right.connecting_line = None
+            merge(child_node_a.left, child_node_a.right)
+        else:
+            print('Child A is a leaf!')
+        if not child_node_b.is_leaf:
+            child_node_b.graph = []
+            child_node_b.heads = []
+            child_node_b.left.connecting_line = None
+            child_node_b.right.connecting_line = None
+            merge(child_node_b.left, child_node_b.right)
+        else:
+            print('Child B is a leaf!')
+
+        # re-do visbility check
+        merge_pairs = [(a, b) for a in child_node_a.heads for b in child_node_b.heads]
+
+        final_merge_pairs = []
+        for p in merge_pairs:
+            p0_coords = [tract_coords[tract_coords.tract_ID == p[0]].X.values[0],
+                         tract_coords[tract_coords.tract_ID == p[0]].Y.values[0]]
+            p1_coords = [tract_coords[tract_coords.tract_ID == p[1]].X.values[0],
+                         tract_coords[tract_coords.tract_ID == p[1]].Y.values[0]]
+            newLine = LineString([p0_coords, p1_coords])
+            # construct ring using a.graph and b.graph and newLine, and see if it forms a ring
+            # if so, remove it from list of candidates
+            ring = MultiLineString(child_node_a.graph + child_node_b.graph + [newLine])
+            if ring.is_simple:
+                final_merge_pairs.append(p)
+
+        merge_pairs = [p for p in final_merge_pairs]
     else:
         # copy lines and heads into parent, plus new line (randomly chosen from merge_pairs)
         if len(merge_pairs) > 1:
@@ -196,6 +225,16 @@ def merge(child_node_a, child_node_b):
     child_node_a.parent.graph_node()
     return child_node_a.parent  # or child_node_b.parent, it really doesn't matter
 
+def merge_fix(node_a, node_b):
+    """
+    Fixes a merge issue when trapped case is detected
+    Args:
+        node_a:
+        node_b:
+
+    Returns:
+
+    """
 
 def merge_tree(root):
     """
@@ -247,29 +286,36 @@ def clustering(tree, tracts):
         clustering(tree, list(group_b.tract_ID.values))
 
 
-# tract_coords = pd.DataFrame({'tract_ID': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-#                                           'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-#                              'X': [1, 2, 4, 3, 5, 10, 0, 9, 0, 0, 7, 5, 2, 7, 0, 9],
-#                              'Y': [3, 1, 3, 5, 6, 11, 9, 0, 1, 0, 3, 10, 8, 8, 10, 5]})
-tract_coords = pd.read_csv('graph_tests/normal_30.csv')
+tract_coords = pd.DataFrame({'tract_ID': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                          'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
+                             'X': [1, 2, 4, 3, 5, 10, 0, 9, 0, 0, 7, 5, 2, 7, 0, 9],
+                             'Y': [3, 1, 3, 5, 6, 11, 9, 0, 1, 0, 3, 10, 8, 8, 10, 5]})
+# tract_coords = pd.read_csv('graph_tests/normal_30.csv')
+
 
 def main(i=None):
-    print(tract_coords.head())
+    # print(tract_coords.head())
     pd.set_option('mode.chained_assignment', None)
     cluster_tree = assemble_tree()
-    rootNode = merge_tree(cluster_tree.root)
-    rootNode.graph_node()
+    cluster_tree.printPostorder(cluster_tree.root)
+    root_node = merge_tree(cluster_tree.root)
+    root_node.graph_node()
+    # print(root_node.heads)
+    if len(root_node.heads) == 0:
+        return 0
+    else:
+        return 1
 
 
 if __name__ == '__main__':
-    # volume = 500
-    # actual = volume
+    volume = 500
+    actual = 0
     # print("Generating {} graphs and saving to 16-points-graphs/...".format(volume))
     # for i in tqdm(range(volume)):
     #     try:
-    #         main(i + 1)
+    #         actual += main(i + 1)
     #     except RuntimeError:
     #         actual -= 1
     #         pass
-    # print("Generation finished, {} generated, {}% accuracy.".format(actual, (actual/volume)*100))
-    main()
+    # print("Generation finished, {} generated, {}% accuracy.".format(actual, (actual / volume) * 100))
+    print(main())
